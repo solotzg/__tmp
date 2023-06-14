7bbe523f050ecefb51092983a95796651f679ac5
solotzg <solotzg@163.com>
2023-06-14 22:03:42 +0800

    34

--- run-flink-bash.sh
+++ run-flink-bash.sh
@@ -8,10 +8,4 @@ SCRIPT_PATH=$(
 )
 COMPOSE_FILE_NAME=".tmp.docker-compose_hadoop_hive_spark_flink.yml"
 
-if [[ -z "${HUDI_WS}" ]]; then
-  echo "ERROR: env var HUDI_WS is empty"
-  exit -1
-fi
-
-# restart cluster
-HUDI_WS=${HUDI_WS} docker compose -f ${SCRIPT_PATH}/${COMPOSE_FILE_NAME} run -it --rm sql-client $@
+docker compose -f ${SCRIPT_PATH}/${COMPOSE_FILE_NAME} run -it --rm sql-client $@
--- run-flink-client-sql.sh
+++ run-flink-client-sql.sh
@@ -8,15 +8,9 @@ SCRIPT_PATH=$(
 )
 COMPOSE_FILE_NAME=".tmp.docker-compose_hadoop_hive_spark_flink.yml"
 
-if [[ -z "${HUDI_WS}" ]]; then
-  echo "ERROR: env var HUDI_WS is empty"
-  exit -1
-fi
-
 if [[ -z "${SQL_PATH}" ]]; then
   echo "ERROR: env var SQL_PATH is empty"
   exit -1
 fi
 
-# restart cluster
-HUDI_WS=${HUDI_WS} docker compose -f ${SCRIPT_PATH}/${COMPOSE_FILE_NAME} run -it --rm sql-client /pingcap/demo/flink-sql-client.sh embedded -f /pingcap/demo/${SQL_PATH}
+docker compose -f ${SCRIPT_PATH}/${COMPOSE_FILE_NAME} run -it --rm sql-client /pingcap/demo/flink-sql-client.sh embedded -f /pingcap/demo/${SQL_PATH}
