f6fe7f66876ea6d858131ab3cacabe26dabab1b3
Zhigao TONG <tongzhigao@pingcap.com>
2026-06-24 18:57:16 +0800

    back4app

--- example2/tidb.sql
+++
@@ -1,3 +0,0 @@
-create database IF NOT EXISTS demo;
-create table IF NOT EXISTS demo.t2(a int PRIMARY KEY, b TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, c varchar(20));
-insert into demo.t2 (a,c) values(1,'1'),(2,'2'),(3, null);
--- flink-taskmanager.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPTPATH="$(
-    cd "$(dirname "$0")"
-    pwd -P
-)"
-
-source ${SCRIPTPATH}/_flink-env.sh
-/docker-entrypoint.sh taskmanager $@
